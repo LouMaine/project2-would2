9c0eb52cace415200fa77a85e558816709597290
Lourdes Wellington <mlwellington@myfairpoint.net>
2019-09-02 00:19:55 -0400

    Add files via upload
    authedUser changes

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,54 +1,50 @@
-import { showLoading, hideLoading } from 'react-redux-loading-bar';
-import { _saveQuestion } from "../utils/_DATA";
-import {addQuestionToUser} from '../actions/users';
+import {showLoading, hideLoading} from "react-redux-loading-bar";
+import { _saveQuestion } from "../_DATA";
+import { QuestionToUser } from "../actions/users";
+//import { AnsweRToQuestion} from "../actions/questions";
 
-export const GET_QUESTIONS = "GET_QUESTIONS";
-export const SAVE_QUESTION = "SAVE_QUESTION";
-export const ADD_ANSWER_TO_QUESTION = "ADD_ANSWER_TO_QUESTION";
+export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
+export const ANSWER_TO_QUESTION = "ANSWER_TO_QUESTION";
+export const ADD_QUESTION = "ADD_QUESTION";
 
-
-
-export const getQuestions = questions => {
-  return ({
-  type: GET_QUESTIONS,
-  questions,
-})
-};
-
-
-
-export const saveQuestion = question => ({
-  type: SAVE_QUESTION,
-  question,
-});
-
-
-export const handleSaveQuestion = (optionOneText, optionTwoText) => (dispatch, getState) => {
-  const { authedUser } = getState();
-
-  dispatch(showLoading());
-
-  return _saveQuestion({
-    optionOneText,
-    optionTwoText,
-    author: authedUser,
-  })
-    .then(formattedData => dispatch(saveQuestion(formattedData)))
-    .then(() => dispatch(hideLoading()));
-};
-
-
-export const addAnswerToQuestion = ({ authedUser, qid, answer }) => ({
-  type: ADD_ANSWER_TO_QUESTION,
-  authedUser,
-  qid,
-  answer
+export const receiveQuestions =questions=> {
+  return {
+  type: RECEIVE_QUESTIONS,
+  questions
 };
 }
-function saveQuestion(question) {
+
+export function AnswerToQuestion(authedUser, qid, answer) {
   return {
-    type: SAVE_QUESTION,
+    type: ANSWER_TO_QUESTION,
+    authedUser,
+    qid,
+    answer
+  };
+}
+
+export function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
     question
   };
 }
 
+export const handleSaveQuestion=(optionOneText, optionTwoText)=> (dispatch, getState) => {
+   const {authedUser} = getState();
+   dispatch(showLoading());
+
+    return _saveQuestion({
+     optionOneText,
+     optionTwoText,
+     author: authedUser
+     })
+     .then(
+      question => {
+      	dispatch(addQuestion(question));
+      	dispatch(QuestionToUser(question));
+        dispatch(hideLoading());  
+      }
+   );
+ };
+   
