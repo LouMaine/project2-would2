a1f51586de1494a865f260902851de81e4851c59
Lourdes Wellington <mlwellington@myfairpoint.net>
2019-09-02 00:21:25 -0400

    Add files via upload
    authedUser changes

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,23 +1,10 @@
-
 import { showLoading, hideLoading } from "react-redux-loading-bar";
-//import {getInitialData} from "../utils/api";
-//import { _getUsers, _getQuestions, _saveQuestionAnswer } from "../_DATA";
-import { _getUsers, _getQuestions, _saveQuestionAnswer } from '../_DATA';
-import { receiveUsers } from "./users";
-import { getQuestions} from "./questions";
-import { addUserAnswer} from "./users";
-import {addAnswerToQestion } from "./actions/questions"
-//import { saveAnswer } from "./questions";
-//import {_getQuestions} from "../utils/api";
+import { _getUsers, _getQuestions } from "../_DATA";
 
-/**export const handleSaveQuestionAnswer = (authedUserId, questionId, answer) => {
- 
-  return  async dispatch => {
-    await _saveQuestionAnswer(authedUserId, questionId, answer);
-    dispatch(handleReceiveUsers());
-    dispatch(handleSaveQuestion());
-  };
-};**/
+//import { getInitialData } from "../utils/api";
+import { receiveQuestions } from "../actions/questions";
+import { receiveUsers } from "../actions/users";
+
 
 
 export const handleInitialData = () => (dispatch) => {
@@ -25,67 +12,7 @@ export const handleInitialData = () => (dispatch) => {
   return Promise.all([_getUsers(), _getQuestions()])
     .then(([users, questions]) => {
       dispatch(receiveUsers(users));
-      dispatch(getQuestions(questions));
+      dispatch(receiveQuestions(questions));
       dispatch(hideLoading());
     });
 };
-
-export const handleSaveAnswer = (qid, answer) => (dispatch, getState) => {
-  const { authedUser } = getState();
-
-  dispatch(showLoading());
-  return _saveQuestionAnswer({
-    authedUser,
-    qid,
-    answer,
-  })
-    .then(() => {
-      dispatch(addAnswerToQestions(authedUser, qid, answer)
-      //dispatch(saveAnswer(authedUser, qid, answer));
-      dispatch(addUserAnswer(authedUser, qid, answer));
-    })
-    .then(() => dispatch(hideLoading()));
-};
-
-
-/*
-
-
-
-
-
-
-
-
-
-*export  const { authedUser } = getState();
-
-  dispatch(showLoading());
-  return _saveQuestionAnswer({
-    authedUser,
-    qid,
-    answer,
-  })
-    .then(() => {
-      dispatch(saveAnswer(authedUser, qid, answer));
-      dispatch(addUserAnswer(authedUser, qid, answer));
-    })
-    .then(() => dispatch(hideLoading()));
-};
-**/
-
-
-/**export const handleSaveQuestion = question => {
-  return async dispatch => {
-    const savedQuestion = await saveQuestion(question);
-    console.log(savedQuestion);
-    dispatch(addQuestion(savedQuestion)); 
-    
-    dispatch(handleReceiveUsers());
- };
-};
-**/
-
-
-
-
