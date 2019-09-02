9710d8c8e5841328ce3eb2bd79ea1a0fe2ea6fe3
Lourdes Wellington <mlwellington@myfairpoint.net>
2019-09-02 00:10:58 -0400

    Add files via upload
    change authedUser

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+//import PropTypes from "prop-types";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import { connect } from "react-redux";
-//import PropTypes from "prop-types";
+
 import { loginAuthedUser } from "../actions/authedUser";
 
 
