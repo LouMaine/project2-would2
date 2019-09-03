ded94a40547f7f2de84237ea73edb9f85acb7883
Lourdes Wellington <mlwellington@myfairpoint.net>
2019-09-03 01:51:02 -0400

    Add files via upload
    add user id  variable

---
+++ QuestionDetails.js
@@ -0,0 +1,118 @@
+ 
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Card, CardHeader, CardBody, CardTitle, FormGroup, Label, Input, Form, Button } from "reactstrap";
+import { FaCheck } from "react-icons/fa";
+import { handleSaveQuestionAnswer } from "../actions/users";
+import Avatar from "./Avatar";
+
+/*** Used FormGroup, Card, CardBody, Labels code from the 'reactstrap' website and 'Github notes'****/
+
+class QuestionDetails extends Component {
+  state = {
+    selectedAnswer: '',
+  }
+
+  radioSelected = (e) => {
+    this.setState({
+      selectedAnswer: e.target.value,
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.AnswerToQuestion(this.state.selectedAnswer);
+  }
+
+  render() {
+    const {questionAuthor, user, question,  isAnswered, isOptionOneAnswered } = this.props;
+
+    if (!question) {
+      return <Redirect to="/ErrorPage" />;
+    }
+
+    const { selectedAnswer } = this.state
+
+    const optionOneVotes = question.optionOne.votes.length;
+    const optionTwoVotes = question.optionTwo.votes.length;
+    const percentageOptionOne = (optionOneVotes / (optionOneVotes + optionTwoVotes) * 100).toFixed(2);
+    const percentageOptionTwo = (optionTwoVotes / (optionOneVotes + optionTwoVotes) * 100).toFixed(2);
+
+    const checkmark = <FaCheck size="5" color="gray" />;
+
+    return (
+         //<questionAuthor = users[question.author] />
+       <Card>
+                       
+        <CardHeader>
+       { /***  Information from reactstrap Github components Cards, Forms Buttons**/}
+          
+          <user id={questionAuthor.id}/>
+                  
+        </CardHeader>
+        <CardBody>
+          <CardTitle>Rather Do...?</CardTitle>
+          {isAnswered
+            ? (
+              <ul>
+              <li>{question.optionOne.text} ({optionOneVotes} vote(s) at {percentageOptionOne}%){isOptionOneAnswered ? checkmark : null}</li>
+              <li>{question.optionTwo.text} ({optionTwoVotes} vote(s) at {percentageOptionTwo}%){isOptionOneAnswered ? checkmark : null}</li>
+              </ul>
+            ) : (
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup tag="fieldset">
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="radio1" value="optionOne" onChange={this.radioSelected} />{' '}
+                    {question.optionOne.text}
+                  </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="radio1" value="optionTwo" onChange={this.radioSelected} />{' '}
+                    {question.optionTwo.text}
+                  </Label>
+                </FormGroup>
+              </FormGroup>
+              <Button disabled={selectedAnswer === ''}>Submit</Button>
+            </Form>
+            )}
+        </CardBody>
+      </Card>
+    );
+  }
+}
+
+
+QuestionDetails.propTypes = {
+  question: PropTypes.object,
+  questionAuthor: PropTypes.object,
+  isAnswered: PropTypes.bool.isRequired,
+  AnswerToQuestion: PropTypes.func.isRequired,
+  isOptionOneAnswered: PropTypes.bool.isRequired,
+};
+
+
+const mapStateToProps = ({ questions, users, authedUser }, props) => {
+  const { id } = props.match.params;
+  const question = questions[id];
+  const questionAuthor = users[question.author];
+  const isOptionOneAnswered = question.optionOne.votes.includes(authedUser);
+  const isOptionTwoAnswered = question.optionTwo.votes.includes(authedUser);
+  const isAnswered = isOptionOneAnswered || isOptionTwoAnswered;
+  return { question, questionAuthor, isAnswered, isOptionOneAnswered,
+  };
+};
+
+const mapDispatchToProps = (dispatch, props) => {
+  const { id } = props.match.params;
+  return {
+    AnswerToQuestion: (answer) => {
+      dispatch(handleSaveQuestionAnswer(id, answer));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionDetails);
