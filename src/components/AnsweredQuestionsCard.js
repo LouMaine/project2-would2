81ce45088f132ca7613e650fbad754cb4eb8ed78
Lourdes Wellington <mlwellington@myfairpoint.net>
2019-09-09 18:57:21 -0400

    Add files via upload
    delete typed error

--- src/components/AnsweredQuestionsCard.js
+++ src/components/AnsweredQuestionsCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import UserQuestionsCard from "./UserQuestionsCard";
+import QuestionDetails from "./QuestionDetails";
 
 export const AnsweredQuestions = props => {
   const { answeredQuestions } = props;
@@ -14,7 +15,7 @@ export const AnsweredQuestions = props => {
               avatarUrl={author.avatarURL}
               name={author.name}
               questionId={question.id}
-              optionsSummaryText={question[answer].text
+              optionsSummaryText={question[answer].text    
                 .substring(0, 20)
                 .concat("...")}
             />
