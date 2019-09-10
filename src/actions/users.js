0a2ba1c65834262946e4afd6ba060e96d95ebe1a
Lourdes Wellington <mlwellington@myfairpoint.net>
2019-09-10 11:37:30 -0400

    Add files via upload
    dup file update

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,51 +1,45 @@
-
-import { RECEIVE_USERS } from "../actions/users";
-import {ANSWER_TO_USER} from "../actions/users";
-import {QUESTION_TO_USER} from "../actions/users";
+import { saveQuestionAnswer } from "../utils/api";
+import { AnswerToQuestion } from "../actions/questions";
 
 
 
 
+export const RECEIVE_USERS = "RECEIVE_USERS";
+export const ANSWER_TO_USER = "ANSWER_TO_USER";
+export const QUESTION_TO_USER= "QUESTION_TO_USER";
 
-export const users = (state={}, action) => {
-  switch (action.type) {
 
-    case RECEIVE_USERS: 
-    return {
-    ...state,
-    ...action.users,
+export const receiveUsers = users => {
+  return {
+    type: RECEIVE_USERS,
+    users
   };
-
-     case ANSWER_TO_USER: 
-      const { authedUser, qid, answer } = action;
-     console.log(state, authedUser);
-      return {
-        ...state,
-        [action.authedUser]: {
-          ...state[action.authedUser],
-               
-          answers: {
-            ...state[action.authedUser].answers,
-            [action.qid]: action.option
-            }
-          }
-        };
-      
-
-      case QUESTION_TO_USER:  
-      const { id, author } = action;
-
-      return {
-        ...state,
-        [action.author]: {
-          ...state[action.author],
-          questions: state[action.author].questions.concat([action.id])
-        }, 
-       
-      }
-
-  default:
-  return state
-  }
 };
-export default users;
+
+export function AnswerToUser(authedUser, qid, option) {
+  return {
+    type: ANSWER_TO_USER,
+    authedUser,
+    qid,
+    option,
+  };
+}
+
+export function handleSaveQuestionAnswer(authedUser, qid, answer) {
+  return dispatch => {
+    dispatch(AnswerToUser(authedUser, qid, answer));
+    dispatch(AnswerToQuestion(authedUser, qid, answer));
+
+    return saveQuestionAnswer(authedUser, qid, answer).catch(e => {
+      console.warn('Error in handleSaveQuestionAnswer:', e);
+    });
+  };
+}
+
+export function QuestionToUser( id, author ) {
+  return {
+    type: QUESTION_TO_USER,
+    id,
+    author,
+  };
+}
