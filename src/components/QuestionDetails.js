0ebf30cad694a7d8880ba7d3938a71eda2eb5f8f
Lourdes Wellington <mlwellington@myfairpoint.net>
2019-09-30 17:00:05 -0400

    Add files via upload
    update change to  state and map dispatch

--- src/components/QuestionDetails.js
+++ src/components/QuestionDetails.js
@@ -1,18 +1,20 @@
- 
-import React, { Component } from "react";
+import React  from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Card, CardHeader, CardBody, CardTitle, FormGroup, Label, Input, Form, Button } from "reactstrap";
 import { FaCheck } from "react-icons/fa";
-import { handleSaveQuestionAnswer } from "../actions/users";
-import Login from "./Login";
+import {AnswerToQuestion} from "../actions/questions";
+import { handleSaveQuestionAnswer } from "../actions/shared";
+import Avatar from "./Avatar";
+import ErrorPage from "./ErrorPage"
 
 /*** Used FormGroup, Card, CardBody, Labels code from the 'reactstrap' website and 'Github notes'****/
-
-class QuestionDetails extends Component {
+//const {question, answer, answers} =qid;
+class QuestionDetails extends React.Component {
   state = {
     selectedAnswer: '',
+   // selectedAnswer: qid,
   }
 
   radioSelected = (e) => {
@@ -24,20 +26,23 @@ class QuestionDetails extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.AnswerToQuestion(this.state.selectedAnswer);
-  };
+  }
 
   render() {
     
-    const { answers, question, option,  questionAuthor, userId, isAnswered, isOptionOneAnswered } = this.props;
-    const {selectedAnswer}=this.state;
-    const answer = selectedAnswer[option];
+    const {authedUser, question, answer, option, User, userId, questionAuthor, isAnswered, isOptionOneAnswered } = this.props;
+   // const {selectedAnswer}=this.state;
+   // const answer = selectedAnswer[option];
+    
    
-
-    if (!question) {
-      return <Redirect to="/ErrorPage" />;
+   if (!question) {
+     return <Redirect to = "/"/>
+       
+    if (authedUser===null)
+      return <Redirect to="/Login"/>
     }
 
-    
+    const {selectedAnswer} =this.state
 
     const optionOneVotes = question.optionOne.votes.length;
     const optionTwoVotes = question.optionTwo.votes.length;
@@ -53,8 +58,8 @@ class QuestionDetails extends Component {
         <CardHeader>
        { /***  Information from reactstrap Github components Cards, Forms Buttons**/}
           <user Id={questionAuthor.id}/>
-               
-        </CardHeader>
+          
+      </CardHeader>
          <CardBody>
           <CardTitle>Rather Do...?</CardTitle>
           {isAnswered
@@ -91,43 +96,69 @@ class QuestionDetails extends Component {
     );
   }
 }
-
-
-QuestionDetails.propTypes = {
+ QuestionDetails.propTypes ={
+  history: PropTypes.object.isRequired,  
+  authedUser: PropTypes.object.isRequired,
+  question_id: PropTypes.object.isRequired,
+  users: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  qid: PropTypes.object.isRequired,
   question: PropTypes.object,
+  questionAnswer: PropTypes.object,
   questionAuthor: PropTypes.object,
+  
   answer: PropTypes.string,
   isAnswered: PropTypes.bool.isRequired,
   AnswerToQuestion: PropTypes.func.isRequired,
   isOptionOneAnswered: PropTypes.bool.isRequired,
+
 };
 
 
-const mapStateToProps = ({ questions, users, authedUser }, {match}) => {
-  const answers = users[authedUser].answers;
-  let answer;
-  const { id } = match.params;
-  const question = questions[id];
-if (answers.hasOwnProperty(question.id)) {
-    answer = answers[question.id]
+const mapStateToProps = ({ authedUser, users, UserId, userId, qid, questions,  answer, selectedAnswer, match}, props) => { 
+ 
+ 
+const {id}=props.match.params;
+const authedUserId=[authedUser];
+
+const question= questions[id];
+
+
+ const user=[users.id].questions;
+  if (users.hasOwnProperty(question.id)) {
+   users=users[id];
   }
 
-  const questionAuthor = users[question.author];
+ 
+ const questionAuthor = users[question.author];
+ const answers=users[authedUser].answers;  
+ if (answers.hasOwnProperty(question.id)) {
+ answer =answers[question.id];
+  }
+
   const isOptionOneAnswered = question.optionOne.votes.includes(authedUser);
   const isOptionTwoAnswered = question.optionTwo.votes.includes(authedUser);
   const isAnswered = isOptionOneAnswered || isOptionTwoAnswered;
   
-  return { question, questionAuthor, answer, isAnswered, isOptionOneAnswered,
-  };
+
+  return { authedUser, users, userId, id, qid,  question, questionAuthor, answer, isAnswered, isOptionOneAnswered,
+ }; 
 };
 
-const mapDispatchToProps = (dispatch, props) => {
+
+const mapDispatchToProps =( dispatch, props) => {
+
   const { id } = props.match.params;
+  
   return {
-    AnswerToQuestion: (answer) => {
-      dispatch(handleSaveQuestionAnswer(id, answer));
+    
+  AnswerToQuestion: (answer) => {
+         
+   dispatch( handleSaveQuestionAnswer( id, answer));
     },
   };
 };
 
+
 export default connect(mapStateToProps, mapDispatchToProps)(QuestionDetails);
+
