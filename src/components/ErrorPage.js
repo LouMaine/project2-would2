b62f785d465a34d005094990cf0f9b307d770372
Lourdes Wellington <mlwellington@myfairpoint.net>
2019-09-30 16:50:19 -0400

    Add files via upload
    add click at info

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,14 +1,20 @@
-import React from "react";
-//import {NavLink} from "react-router-dom";
+import React, {Fragment} from "react";
+import {NavLink} from "react-router-dom";
 import { Card, CardBody, CardTitle } from "reactstrap";
+
+
 export const ErrorPage = () => {
   return (
+  	<Fragment>
     <Card>
-      <CardTitle>404 error</CardTitle>
+      <CardTitle>   ***404 error***</CardTitle>
       <CardBody>
         Sorry, the page does not exist
+        <NavLink to="/">  Cick Here  </NavLink>
+        {" "} *** Return to Home Page and Login ***
       </CardBody>
     </Card>
+    </Fragment>
   );
 };
 export default ErrorPage;
