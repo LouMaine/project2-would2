8103045e4b3d5d2408f8a59d9e24a591c1a4d7c7
Lourdes Wellington <mlwellington@myfairpoint.net>
2019-09-30 17:16:29 -0400

    Add files via upload
    update reloads

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 //import './index.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 //import App from "./components/App";
