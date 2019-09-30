0f304b7c475cb238f6bd97ea932a4f5da5b1caf9
Lourdes Wellington <mlwellington@myfairpoint.net>
2019-09-30 17:01:36 -0400

    Add files via upload
    User card item changes

--- src/components/UnansweredQuestionsCard.js
+++ src/components/UnansweredQuestionsCard.js
@@ -1,73 +1,51 @@
-import React from "react";
-import { connect } from "react-redux";
-import UserQuestionsCard from "./UserQuestionsCard";
-//import QuestionDetails from "./QuestionDetails";
-//import {handleSaveQuestion} from "./actions/questions"
-//import {QuestionToUser} from "./actions/users";
-//import {AnswerToUser} from "./actions/users"
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { Redirect } from "react-router-dom";
+import {Button} from "reactstrap";
+//import { colors } from "../utils/helpers";
 
-export const UnansweredQuestions = props => {
-  const { answer, unansweredQuestions } = props;
-  const {users} = this.props;
-    const {userId} =this.state;
-    
-  return (
-    <React.Fragment>
-
-      {unansweredQuestions.map(unansweredQuestion => {
-       const {  userId, answer, question, author, qid,  }=unansweredQuestion;
-        //const authedUser= state.users.find(user => user.id === question.author);
-       console.log(.name);
-        return (
-          <React.Fragment key={question.id}>
-            <UserQuestionsCard
-              avatarUrl={users.avatarURL}
-              name={users.name}
-              questionId={question.id}
-              optionsSummaryText={question.optionOne.text
-                .substring(0, 20)
-                .concat("...")}
-            />
-          </React.Fragment>
-        );
-      })}
-    </React.Fragment>
-  );
-};
-
-const mapStateToProps = state => {
-  const authedUser = state.users.find(user => user.id === state.authedUserId);
-  const unansweredQuestions = [];
-  const unansweredQuestions = Object.keys(authedUser.answers).map(key => {
-   const question = state.questions.find(question => question.id === key);
-  
-  
-                    
-  state.questions.forEach(question => {
-    let found = false;
-    answeredQuestions.forEach(answeredQuestion => {
-      if (question.id === answeredQuestion.id) {
-        found = true;
-      }
-    });
-    if (!found) {
-      if (
-        unansweredQuestions.find(
-          unansweredQuestion => unansweredQuestion.id === question.id
-        ) === undefined
-      ) {
-        unansweredQuestions.push(question);
-      
-      }
-    }
-  });
-
-  return {
-    users: state.users,
-    unansweredQuestions: unansweredQuestions.sort((a,b)=>{
-      return b.question.timestamp-a.question.timestamp;
-    })
+export class UnansweredQuestionsCard extends Component {
+  static propTypes = {
+    question: PropTypes.object.isRequired,
+    unanswered: PropTypes.bool.isRequired
   };
-};
+  state = {
+    viewPoll: false
+  };
+  handleClick = e => {
+    this.setState(prevState => ({
+      viewPoll: !prevState.viewPoll
+    }));
+  };
+  render() {
+    const { question, unanswered } = this.props;
+   // const buttonColor = unanswered === true ? colors.green : colors.blue;
+    const buttonContent = unanswered === true ? 'Answer Poll' : 'Results';
 
-export default connect(mapStateToProps)(UnansweredQuestions);
+    if (this.state.viewPoll === true) {
+      return <Redirect push to={`/questions/${question.id}`} />;
+    }
+    return (
+      <Fragment>
+      <Card>
+      <CardBody>
+        <CardTitle> textAlign="left">
+          Would you rather
+        </CardTitle>
+        <p style={{ textAlign: 'center' }}>
+          {question.optionOne.text}
+          <br />
+          or...
+        </p>
+        <Button 
+          color= "primary" size= "tiny" onClick={this.handleClick} content={buttonContent}>unanswered</Button>          content={buttonContent}
+        />
+        
+        </CardBody>
+        </Card>
+      </Fragment>
+    );
+  }
+}
+
+export default UnansweredQuestionsCard;
