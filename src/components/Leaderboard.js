6a750997fabb290524150d55cd1e61846ddf54b9
Lourdes Wellington <mlwellington@myfairpoint.net>
2019-09-30 16:53:06 -0400

    Add files via upload
    add table code

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,15 +1,51 @@
 import React, { Component, Fragment } from "react";
-import NavItems from "./NavItems";
+import { connect } from "react-redux";
+import {Redirect} from "react-router-dom";
+import { Table } from 'reactstrap';
+import PropTypes from 'prop-types';
 
-class Leaderboard extends Component {
-  render() {
-    return (
-      <Fragment>
-        <NavItems />
-        <div>Leaderboard</div>
-      </Fragment>
-    );
-  }
+function Leaderboard(props) {
+  const { users } = props;
+  return (
+    <Fragment>
+          <h1 className="text-center">Avatar Leaderboard Rank</h1>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Avatar Profile</th>
+            <th>User</th>
+            <th>Questions Asked By Avatar</th>
+            <th>Questions Answered By Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={user.id}>
+              <td>{index + 1}</td>
+              <td><img src={user.avatarURL} className='avatar' alt={`Avatar of ${user.name}`}/></td>
+              <td>{user.name}</td>
+              <td>{user.questions.length}</td>
+              <td>{Object.keys(user.answers).length}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Fragment>
+  )
 }
 
-export default Leaderboard;
+Leaderboard.propTypes = {
+  users: PropTypes.array.isRequired
+};
+
+const mapStateToProps = ({ users }) => {
+  const userScore = user =>
+    Object.keys(user.answers).length + user.questions.length;
+  return {
+    users: Object.values(users).sort((a, b) => userScore(b) - userScore(a))
+  }
+};
+
+export default connect(mapStateToProps)(Leaderboard)
