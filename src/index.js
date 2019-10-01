83c8d6917ebce88614512bf50d9e78320b9794e1
Lourdes Wellington <mlwellington@myfairpoint.net>
2019-10-01 16:06:55 -0400

    Add files via upload
    installed bootstrap per reviewer index not updated; did yarn add --save.....
    npm install --save bootstrap@^4.0.0-alpha.6  react-bootstrap@^0.32.1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-//import './index.css';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-//import App from "./components/App";
-
 import rootReducer from "./reducers/index";
 import middleware from "./middleware";
 import 'bootstrap/dist/css/bootstrap.min.css';
